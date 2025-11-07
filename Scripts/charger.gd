43c2e0cd00521589b758b7be08f4e45f013b9e47
tnorrisoliva <tcno.ae42@gmail.com>
2025-11-06 19:12:54 -0500

    lookat added

---
+++ Scripts/charger.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 250.0
+#MAKE SURE PLAYER IN THE INSPECTOR IS SET TO THE PLAYER NODE
+@export var player : Node2D
+#Hostile or idle Bool
+var Hostile : bool = false
+
+func _physics_process(delta: float) -> void:
+	if Hostile:
+		charge()
+	
+
+	#move_and_slide()
+#create a function for hostile state
+func charge():
+	var direction = (player.position - position).normalized()
+	velocity = direction * SPEED
+	look_at(player.position)
+	move_and_slide()
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		Hostile  = true
+	
