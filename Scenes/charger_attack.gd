e930319d8376618f7fb6fe362552e07db0285489
tnorrisoliva <tcno.ae42@gmail.com>
2025-12-02 16:28:00 -0500

    take damage in player

--- Scenes/charger_attack.gd
+++ Scenes/charger_attack.gd
@@ -20,4 +20,4 @@ func _process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		print("attacking player")
-		#body.take_damage(weapon_damage)
+		body.take_damage(weapon_damage)
