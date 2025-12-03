bbd7124e4c6f2ff2bdf04452931126b9f76f3b91
PierceFrancis <152822455+PierceFrancis@users.noreply.github.com>
2025-12-02 20:00:34 -0500

    Flying swords

--- Scripts/thrown_sword.gd
+++ Scripts/thrown_sword.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var speed: float = 1200.0
+@export var speed: float = 1500.0
 @export var pickup_made: PackedScene = preload("res://Scenes/weapon_pickup.tscn")
  
 
@@ -8,6 +8,12 @@ var direction: Vector2 = Vector2.ZERO
 var ignore_body: Node = null
 
 func _ready() -> void:
+	$slash/AnimationPlayer.play("flying")
+	var clone = $slash.duplicate()
+	clone.scale.x = -1
+	add_child(clone)
+	clone.get_node("AnimationPlayer").play("flying")
+	
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	# Optional: auto-despawn after 2 seconds
 	await get_tree().create_timer(1.0).timeout
@@ -15,13 +21,14 @@ func _ready() -> void:
 		_spawn_pickup()
 		queue_free()
 
+
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
 
 func _on_body_entered(body: Node) -> void:
 	if body == ignore_body:
 		return
-	if body.is_in_group(""):
+	if body.is_in_group("enemy"):
 		# Example: reduce health, trigger effect, etc.
 		GameManager.lives -= 1
 		print("Hit player:", body.name)
