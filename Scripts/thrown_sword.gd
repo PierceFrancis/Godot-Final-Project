b4ea590a3553e2d1d43314702af3377725f278f9
PierceFrancis <152822455+PierceFrancis@users.noreply.github.com>
2025-12-03 21:54:25 -0500

    Flying Sword

--- Scripts/thrown_sword.gd
+++ Scripts/thrown_sword.gd
@@ -1,18 +1,16 @@
-extends Area2D
+extends Node2D
 
 @export var speed: float = 1500.0
 @export var pickup_made: PackedScene = preload("res://Scenes/weapon_pickup.tscn")
  
-
+var weapon_damage: float = 1.0
 var direction: Vector2 = Vector2.ZERO
 var ignore_body: Node = null
 
 func _ready() -> void:
 	$slash/AnimationPlayer.play("flying")
-	var clone = $slash.duplicate()
-	clone.scale.x = -1
-	add_child(clone)
-	clone.get_node("AnimationPlayer").play("flying")
+	$slash2/AnimationPlayer.play("flying2")
+	
 	
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	# Optional: auto-despawn after 2 seconds
@@ -25,23 +23,23 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
 
-func _on_body_entered(body: Node) -> void:
-	if body == ignore_body:
-		return
-	if body.is_in_group("enemy"):
-		# Example: reduce health, trigger effect, etc.
-		GameManager.lives -= 1
-		print("Hit player:", body.name)
-		_spawn_pickup()
-		queue_free()
-	else:
-		_spawn_pickup()
-		queue_free()
-		
-	
+
+
 func _spawn_pickup() -> void:
 	if not pickup_made:
 		return
 	var pickup = pickup_made.instantiate()
 	get_tree().root.add_child(pickup)
 	pickup.global_position = global_position
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		# Example: reduce health, trigger effect, etc.
+		body.take_damage(weapon_damage)
+		print("Hit player:", body.name)
+		_spawn_pickup()
+		queue_free()
+	else:
+		_spawn_pickup()
+		queue_free()
